2b2543b22543f1092cfb5631409067ec192160fe
khizerdev <46632721+khizerdev@users.noreply.github.com>
2022-03-13 20:24:41 +0500

    Layout Added

--- src/App.js
+++ src/App.js
@@ -8,17 +8,70 @@ import {
 } from "react-router-dom";
 import Home from './pages/Home';
 import Product from './pages/Product';
+import { Layout, Menu } from 'antd';
+import {
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+} from '@ant-design/icons';
+import React, { useState } from 'react';
 // import your route components too
 
 function App() {
+
+  const [collapsed, setCollapsed] = useState(false)
+
+  const { Header, Sider, Content } = Layout;
+
+  const toggle = () => {
+    setCollapsed(!collapsed)
+  }
+
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/home" element={<Home />}/>
-          <Route path="/products" element={<Product />}/>
-        </Routes>
-      </BrowserRouter>
+      <Layout>
+        <Sider trigger={null} collapsible collapsed={collapsed}>
+          <div className="logo" />
+          <Menu theme="dark" mode="inline" defaultSelectedKeys={['1']}>
+            <Menu.Item key="1" icon={<UserOutlined />}>
+              nav 1
+            </Menu.Item>
+            <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+              nav 2
+            </Menu.Item>
+            <Menu.Item key="3" icon={<UploadOutlined />}>
+              nav 3
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Header className="site-layout-background" style={{ padding: 0 }}>
+            {React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+              className: 'trigger',
+              onClick: toggle,
+            })}
+          </Header>
+          <Content
+            className="site-layout-background"
+            style={{
+              margin: '24px 16px',
+              padding: 24,
+              minHeight: 280,
+            }}
+          >
+             <BrowserRouter>
+              <Routes>
+                <Route path="/home" element={<Home />}/>
+                <Route path="/products" element={<Product />}/>
+              </Routes>
+            </BrowserRouter>
+
+          </Content>
+        </Layout>
+      </Layout>
+     
     </>
   );
 }
