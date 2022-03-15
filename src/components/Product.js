f47087f439976470762b2428ab77d37acfddf765
khizerdev <46632721+khizerdev@users.noreply.github.com>
2022-03-15 13:41:48 +0500

    Product Image Responsive

--- src/components/Product.js
+++ src/components/Product.js
@@ -7,7 +7,7 @@ function Product({item}) {
   return (
     <div className='item'>
         <h4 className='name'>{item.name}</h4>
-        <img src={item.image} alt="" height='100' width='100'/>
+        <img src={item.image} alt="" className='img-fluid'/>
         <h4 className='price'><b>Price : </b>{item.price} $/-</h4>
         <div className="d-flex justify-content-end">
             <Button >Add To Cart</Button>
